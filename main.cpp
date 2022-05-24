91b92e7abc4cf382e9818e983bd76076b516facb
amirameri93 <100429387+amirameri93@users.noreply.github.com>
2022-05-24 11:53:55 +0430

    t

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int n,l=0;
+	cout<<"adad ra vared konid"<<endl;
+	cin>>n;
+	for(int i=2;i<n;i++)
+	{if(n%i==0)
+	
+	{cout<<"adad aval nist.";
+	l=1;
+	break;
+	}	
+
+}
+	if(l==0) 
+	 {
+		cout<<"adad aval ast";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
